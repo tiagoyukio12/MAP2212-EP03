72c4d2009cfd2d00376d076e5fa4bb12703da0c6
Tiago Y. Fujii <tiagoyukio12@gmail.com>
2019-04-25 15:16:56 -0300

    Plot Beta distribution histogram

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import sobol_seq
+import chaospy
 import matplotlib.pyplot as plt
 
 sobolSeq = sobol_seq.i4_sobol_generate(2, 25000)
@@ -13,4 +14,11 @@ x = sobolSeq[:, 0]
 y = sobolSeq[:, 1]
 
 plt.scatter(x, y, s=1)
+plt.show()
+
+a = 1
+b = 1.5
+distribution = chaospy.Beta(a, b)
+betaSeq = distribution.sample(25000, 'S')
+plt.hist(betaSeq, bins=100)
 plt.show()
